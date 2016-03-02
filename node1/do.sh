71461b3f696151a107adec0628ef9707a3fb78d4
Jean-Paul Laberge <jean-paul.laberge@bell.ca>
2016-03-01 21:24:42 -0500

    first commit

---
+++ node1/do.sh
@@ -0,0 +1,45 @@
+export hn=node1
+
+export IMAGES=/var/lib/libvirt/images
+export CONFIG_DRIVE=./CONFIG-DRIVE
+export ISOs=./ISOs
+
+mkisofs -R -V config-2 -o $ISOs/drive-ubuntu.iso $CONFIG_DRIVE
+mkdir  -p $IMAGES/$hn
+cp $ISOs/drive-ubuntu.iso $IMAGES/$hn
+
+qemu-img create -f qcow2 -b $IMAGES/trusty-server-cloudimg-amd64-disk1.img $IMAGES/$hn.qcow2 50G
+
+virt-install \
+--name "$hn" \
+--virt-type kvm \
+--vcpus 4 \
+--ram 8192 \
+--os-type=linux \
+--os-variant=ubuntutrusty \
+--disk path=$IMAGES/$hn.qcow2 \
+--disk path=$ISOs/drive-ubuntu.iso \
+--nonetwork \
+--graphics vnc,listen=0.0.0.0 \
+--noautoconsole \
+--import \
+--noreboot
+
+cat <<EOF > ./if0.xml
+   <interface type='bridge'>
+      <source bridge='ext'/>
+      <vlan>
+        <tag id='20'/>
+      </vlan>
+      <virtualport type='openvswitch'>
+      </virtualport>
+      <target dev='eth0-$hn'/>
+      <model type='virtio'/>
+      <alias name='net0'/>
+    </interface>
+EOF
+virsh attach-device $hn if0.xml --config
+
+virsh start $hn
+
+
